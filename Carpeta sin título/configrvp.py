e4a07ed1d849cb9ed7ad2553753690fd4fc50655
rvicinip <rvicinip@gmail.com>
2021-09-09 07:28:29 -0500

    Puntoycomaback 0.00a

--- Carpeta sin título/configrvp.py
+++ Carpeta sin título/configrvp.py
@@ -7,9 +7,9 @@ SECRET_KEY = 'F89312C2F1CF2E8428EE4AB3C5968A2B'
 ### URL de la conexión de la DB
 MONGO = "mongodb+srv://VenaUser:C0l0mbi421@cluster0.0jieb.mongodb.net/BPM?retryWrites=true&w=majority"
 ##MONGO = "mongodb+srv://venauser:C0l0mbi421@cluster0.e02uf.mongodb.net/BPM?retryWrites=true&w=majority"
-MYSQL = "mysql+pymysql://{username}:{password}@{server}/{dbname}".format(
-                                                             username ='bpmdb', 
-                                                             password = 'D354rr0ll05V3n4', 
+MYSQL = "mysql+pymysql://{username}:{password}@{server}/{dbname}".format( 
+                                                             username ='rvicini', 
+                                                             password = 'N1v3g1t4r1', 
                                                              server = "localhost",
                                                              dbname = 'bpmdb')
 ### DB Nombre de la DB que utiliza el sistema
